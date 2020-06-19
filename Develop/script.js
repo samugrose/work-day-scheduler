42df884711fabc917f504417836abf9a080c6fb4
Sam Grose <samugrose@gmail.com>
2020-06-19 13:40:04 -0700

    added localstorage functionality to savebtn

--- Develop/script.js
+++ Develop/script.js
@@ -4,17 +4,29 @@ for (i = 9; i <= 17; i++) {
         var hoursec = jQuery( `[value = ${i}]` );
         hoursec.text(i + "AM");
         console.log(hoursec.attr("value"));
+        getStored(i - 9);
     //$(`.container .row .hour:nth-child(${i})`).text(`${i + 8}AM`);
     }else  if (i === 12){
         var hoursec = jQuery( `[value = ${i}]` );
         hoursec.text(i + "PM");
         console.log(hoursec.attr("value"));
+        getStored(i - 9);
     }else { //i> 12
        // $(`.container .row .hour:nth-child(${i})`).text(`${i + 8}PM`);
        var hoursec = jQuery( `[value = ${i}]` );
        hoursec.text((i - 12) + "PM");
         console.log(hoursec.attr("value"));
+        getStored(i - 9);
     }
+}
+
+function getStored(index) {
+var value;
+    input = localStorage.getItem("" + index);
+    if (input !== null) {
+        value = input;
+    }
+    console.log("input value: " + input + " " + index);
 }
 
 
@@ -37,14 +49,13 @@ $( ".container" ).on( "click", ".textarea", function( event ) {
 
 $( ".container" ).on( "click", ".saveBtn", function( event ) {
     event.preventDefault();
-    //v//ar dataVal = $().attr("data-value"); //get a referenece to the textarea with data val of substring 1, 2
-    var saveVal = $(this).attr("data-value").substr(1, 2);
+    var saveVal = JSON.stringify($(this).attr("data-value").substr(1, 2));
     console.log(saveVal);
-    var textRef = jQuery( `[data-value = ${saveVal}]` ).val(); //make save button as submit for text area
-    console.log("text value: " + textRef);
-    //var input = prompt("Event to add: "); //textarea = 1, savebtn = 01 for dataval
-    //textRef.text(input);
-    //make it so you can type into the box directly, hit enter to save
+    var textRef = JSON.stringify(jQuery( `[data-value = ${saveVal}]` ).val()); 
+    console.log("text value: " + textRef + " " + saveVal);
+    localStorage.setItem(saveVal, textRef);
+    var parsed = JSON.parse(localStorage.getItem(saveVal));
+    console.log("saved value: " + parsed);
 });
 
 
